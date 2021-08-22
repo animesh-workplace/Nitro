2d42e6363b3388ab35d0d1977d586c3bc297bf7e
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-08-23 00:00:03 +0530

    updated file - backend/authentication/models.py

--- backend/authentication/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Nitro(models.Model):
-    Job = models.CharField(max_length=255)
-    Sample = models.CharField(max_length=255)
-    Status = models.CharField(max_length=255)
-    Time = models.DateTimeField(auto_now=True)
