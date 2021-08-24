2cae420e581570731cfbe52fa85461080eef1253
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-08-25 00:00:02 +0530

    updated file - pages/index.vue

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <!-- <Tutorial /> -->
+<!--         <div class="grid grid-cols-3 gap-4">
+            <ChartsRecombinant class="col-start-2 col-span-2" />
+        </div> -->
+        <RtableTable />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'IndexPage',
+}
+</script>
