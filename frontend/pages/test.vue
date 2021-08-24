e7a3295bb8266e8238f710cd43cae018a3d163c5
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-08-25 00:00:01 +0530

    updated file - frontend/pages/test.vue

---
+++ frontend/pages/test.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <!-- <Tutorial /> -->
+        <!--         <div class="grid grid-cols-3 gap-4">
+            <ChartsRecombinant class="col-start-2 col-span-2" />
+        </div> -->
+        <RtableTable />
+        <nuxt-link to="/">go back </nuxt-link>
+    </div>
+</template>
+
+<script>
+// import SuperTokens from 'supertokens-website'
+
+export default {
+    name: 'IndexPage',
+    mounted() {
+        this.$nextTick(async () => {
+            // SuperTokens.init({
+            //     apiDomain: 'http://localhost:3000',
+            //     apiBasePath: '/auth',
+            // })
+            // console.log(await SuperTokens.doesSessionExist())
+        })
+    },
+}
+</script>
