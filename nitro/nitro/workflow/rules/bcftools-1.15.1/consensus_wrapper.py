851a6074f4cd4a493e4eb1c57c235ad853435e0e
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-05-13 10:09:43 +0530

    wrapper for rule bcftools consensus

---
+++ nitro/nitro/workflow/rules/bcftools-1.15.1/consensus_wrapper.py
@@ -0,0 +1,32 @@
+import requests
+from datetime import datetime
+from snakemake.shell import shell
+
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+
+requests.post(
+    "http://localhost:5000/print",
+    data={
+        "task": "BCFTools Consensus",
+        "status": "Started",
+        "time": str(datetime.now()),
+        "db_loc": snakemake.config["db_loc"],
+        "sample": f"{snakemake.wildcards.sample}",
+    },
+)
+shell(
+    """
+        bcftools consensus -f {snakemake.params.reference:q} {snakemake.input.vcf:q} -o {snakemake.output[0]:q} {log}
+        sed -i "1s/.*/>{snakemake.wildcards[0]}/" {snakemake.output[0]:q}
+    """
+)
+requests.post(
+    "http://localhost:5000/print",
+    data={
+        "task": "BCFTools Consensus",
+        "status": "Finished",
+        "time": str(datetime.now()),
+        "db_loc": snakemake.config["db_loc"],
+        "sample": f"{snakemake.wildcards.sample}",
+    },
+)
